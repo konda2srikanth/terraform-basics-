6ffba328182f36879dafa030d14572bbc088ede6
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-11 15:19:46 +0530

    Auto Push

---
+++ 05-loops/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "main" {
+    count = 3
+}
+
+resource "null_resource" "fruits" {
+    count = length(var.fruits)
+}
+
+variable "fruits" {
+    default = ["orange" , "peer" , "grapes"]
+}
--- Expense-v1/ec2.tf
+++ Expense-v1/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0fcc78c828f981df2"
   instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-057338d08bf482782"]
 
   tags = {
     Name = "frontend"
@@ -10,6 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-0fcc78c828f981df2"
   instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-057338d08bf482782"]
 
   tags = {
     Name = "mysql"
@@ -19,7 +21,7 @@ resource "aws_instance" "mysql" {
 resource "aws_instance" "backend" {
   ami           = "ami-0fcc78c828f981df2"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids=["sg-057338d08bf482782"]
   tags = {
     Name = "backend"
   }
