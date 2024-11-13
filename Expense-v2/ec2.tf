3dc4f8d93959a22ce51a71e217e8c2245700e745
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-13 21:15:17 +0530

    Auto Push

--- Expense-v2/ec2.tf
+++ Expense-v2/ec2.tf
@@ -1,28 +1,10 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "main" {
+  count =3
   ami           = "ami-0fcc78c828f981df2"
   instance_type = "t2.micro"
   vpc_security_group_ids=["sg-057338d08bf482782"]
 
   tags = {
-    Name = "frontend"
-  }
-}
-
-resource "aws_instance" "mysql" {
-  ami           = "ami-0fcc78c828f981df2"
-  instance_type = "t2.micro"
-  vpc_security_group_ids=["sg-057338d08bf482782"]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "backend" {
-  ami           = "ami-0fcc78c828f981df2"
-  instance_type = "t2.micro"
-  vpc_security_group_ids=["sg-057338d08bf482782"]
-  tags = {
-    Name = "backend"
+    Name = "frontend-${count.index}"
   }
 }
