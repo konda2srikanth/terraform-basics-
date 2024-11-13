b78bd195e5cabb853305bae14f843a8a283b9104
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-13 22:47:51 +0530

    Auto Push

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 resource "aws_instance" "main" {
   ami           = "ami-0fcc78c828f981df2"
   instance_type = var.env == dev ? t2.micro : t2.medium
-  
     tags = {
     Name = "ec2"
   }
