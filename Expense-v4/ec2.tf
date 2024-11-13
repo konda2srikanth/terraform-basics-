3f1f6e2f66a56ad9ed65a9c422244a745bdf2afb
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-13 22:57:32 +0530

    Auto Push

---
+++ Expense-v4/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "main" {
+  for_each =var.components
+  ami           = "ami-0fcc78c828f981df2"
+  instance_type = each.value["instance_type"] == ".*" ? each.value["instance_type"] : "t2.micro"
+  vpc_security_group_ids=["sg-057338d08bf482782"]
+
+  tags = {
+    Name = each.key
+    bu = each.value["bu"]
+  }
+}
+
+variable "components" {
+  default = {
+    frontend = { 
+      instance_type = "t2.micro"
+      bu = "development"
+    }
+    backend = {
+      instance_type = "t2.micro"
+      bu = "development"
+    }
+    mysql = {
+      #instance_type = "t2.micro"
+      bu = "dba"
+    }
+  }
+}
+
+
