24917bcd67ec06435b25361c8bba4a7eb71a9842
Konda Srikanth <konda2srikanth@gmail.com>
2024-12-21 22:08:11 +0530

    Auto Push

---
+++ 04-ec2/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "main" {
+  count = 3
+  ami           = "ami-0583d8c7a9c35822c"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-057338d08bf482782"]
+
+  tags = {
+    Name = "Frontend"
+  }
+}
