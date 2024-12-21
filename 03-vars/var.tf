5bd05d4a7c8423a8cece684344fb9e525237254d
Konda Srikanth <konda2srikanth@gmail.com>
2024-12-21 20:19:35 +0530

    Auto Push

--- 03-vars/var.tf
+++ 03-vars/var.tf
@@ -18,3 +18,17 @@ output "sample_op" {
     value = " ${var.sample[0]} and ${var.sample[1]} "
   
 }
+
+variable "m" {
+    default = {
+        Name = "mike" ,
+        Content = "DevOps" ,
+        slary = 10000
+    }
+  
+}
+
+output "my" {
+    value = var.m
+  
+}
